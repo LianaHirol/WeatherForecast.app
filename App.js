7db29fc904298d2051f5885d6ea0fc49dd5be0be
LianaHirol <121199810+LianaHirol@users.noreply.github.com>
2023-05-12 22:24:09 -0600

    Update App.js

--- App.js
+++ App.js
@@ -10,23 +10,15 @@ export default function App() {
 
         <footer>
           This project was coded by{" "}
-          <a href="https://www.delac.io/" target="_blank">
-            Matt Delac
-          </a>{" "}
+         Liana
+    
           and is{" "}
-          <a
-            href="https://github.com/wecodeschool/react-weather-app"
-            target="_blank"
-          >
+         
+        
             open-sourced on GitHub
-          </a>{" "}
-          and{" "}
-          <a
-            href="https://suspicious-beaver-111c4d.netlify.com/"
-            target="_blank"
-          >
+         
             hosted on Netlify
-          </a>
+          
         </footer>
       </div>
     </div>
